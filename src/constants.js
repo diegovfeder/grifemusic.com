15335ca4be18df0471a6d9019a5f4b353199d18c
DIEGO FEDER <63731464+diegovfeder@users.noreply.github.com>
2021-12-06 17:13:59 -0300

    Updated master!!!

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export default {
   TITLE: `Listen to our remix of Lost Desert's ~Float Some More~`,
-  SUB_TITLE: `We dit it in collaboration with Albuquerque and it's now released on Warung Recordings 🙌`,
+  SUB_TITLE: `We did it in collaboration with Albuquerque and it's now released on Warung Recordings 🙌`,
   DOWNLOAD_LINK: 'https://www.beatport.com/release/float-some-more/3571516',
 }
